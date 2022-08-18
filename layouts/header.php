70ccffd0f29d458bbfd696a63ec4185263ad3ba4
Sergio Garcia <checoubuntu@gmail.com>
2022-08-18 01:34:53 -0500

    Commit inicial

---
+++ layouts/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Aprendiendo</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./assets/css/footer.css">
+        <link rel="stylesheet" href="./assets/css/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+        <script type="text/javascript" src="./assets/js/mobile.js"></script>
+    </head>
+    <body>
+        <header>
+            <a class="logo" href="index.php"><h2>Aprendiendo</h2></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Cursos</a></li>
+                    <li><a href="#">¿Quienes somos?</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="#">Registrate</a>
+            <p class="menu cta">Menu</p>
+        </header>
+        <div id="mobile__menu" class="overlay">
+            <a class="close">&times;</a>
+            <div class="overlay__content">
+                <a href="#">Cursos</a>
+                <a href="#">¿Quienes somos?</a>
+                <a href="#">Registrate</a>
+            </div>
+        </div>
