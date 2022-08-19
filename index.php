61eb19f0142a0cdbfe947f19021a694ce928e392
Sergio Garcia <checoubuntu@gmail.com>
2022-08-18 19:07:36 -0500

    Empezar de nuevo

--- index.php
+++ index.php
@@ -1,34 +1,43 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Navbar</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./assets/css/styles.css">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <header>
-            <a class="logo" href="/"><img src="images/logo.svg" alt="logo"></a>
-            <nav>
-                <ul class="nav__links">
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">About</a></li>
-                </ul>
-            </nav>
-            <a class="cta" href="#">Contact</a>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Navbar</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Encabezado - Navegacion -->
+    <header>
+        <a class="logo" href="index.php"><h2>Aprendiendo</h2></a>
+        <nav>
+            <ul class="nav__links">
+               <li><a href="#">Cursos</a></li>
+               <li><a href="#">¿Quienes somos?</a></li>
+            </ul>
+        </nav>
+            <a class="cta" href="#">Registrate</a>
             <p class="menu cta">Menu</p>
-        </header>
+    </header>
         <div id="mobile__menu" class="overlay">
             <a class="close">&times;</a>
             <div class="overlay__content">
-                <a href="#">Services</a>
-                <a href="#">Projects</a>
-                <a href="#">About</a>
+                <a href="#">Cursos</a>
+                <a href="#">¿Quienes somos?</a>
+                <a href="#">Registrate</a>
             </div>
         </div>
-        <script type="text/javascript" src="./assets/js/mobile.js"></script>
-    </body>
+<!-- Presentación -->
+    <section id="hero">
+            <h1 class="text">Aprende a Programar <br> ¡Totalmente grátis!</h1>
+            <button class="text">¡Registrate!</button>
+    </section>
+    <script type="text/javascript" src="./assets/js/mobile.js"></script>
+</body>
+
 </html>
